9d0f5b3c23cc4beed0db40e41aaddc61ae96aaa7
viettd <viet751993@gmail.com>
2018-09-26 10:33:36 +0700

    remove node-ipc

--- index.js
+++ index.js
@@ -1,48 +1,18 @@
-const ipc = require('node-ipc');
-
-const init = function (cb) {
-
-    ipc.config.id = Math.random() * 1000;
-    ipc.config.retry = 1500;
-
-    ipc.connectTo(
-        'aCheckinMain',
-        function () {
-            if (cb) cb({
-                on: function (event, fn) {
-                    ipc.of.aCheckinMain.on(event, fn)
-                },
-                emit: function (event, data) {
-                    ipc.of.aCheckinMain.emit(event, data);
-                }
-            })
-        }
-    );
-}
-
-const initPromise = function () {
-    return new Promise(function (resolve, reject) {
-        init(function ({on, emit}) {
-            resolve({
-                on,
-                openDoor: function(doorNumber){
-                    emit("request-open-door", doorNumber)
-                },
-                closeDoor: function (doorNumber) {
-                    emit("request-close-door", doorNumber)
-                }
-            });
-        })
+const openDoor = function (doorCode) {
+    process.send({
+        event: 'request-open-door',
+        data: doorCode
     });
 }
 
-initPromise().then(function ({ on, emit }) {
-    on(
-        'open-door',  //any event or message type your server listens for
-        function (data) {
-            ipc.log('got a message from world : '.debug, data);
-        }
-    );
-})
+const closeDoor = function (doorCode) {
+    process.send({
+        event: 'request-close-door',
+        data: doorCode
+    });
+}
 
-module.exports = initPromise;
+module.exports = {
+    openDoor,
+    closeDoor
+};
